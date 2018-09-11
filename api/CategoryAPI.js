ba5aeb5255a2abaaf17d170919249aef91ba1536
zs7203 <hellozs@126.com>
2018-09-11 16:05:16 +0800

    update RootQuery [channel]

--- api/CategoryAPI.js
+++ api/CategoryAPI.js
@@ -1,18 +1,21 @@
-const {catetory: Category} = require('./api')
-const {RESTDataSource} = require('apollo-datasource-rest')
+const { catetory: Category } = require('./api')
+const { RESTDataSource } = require('apollo-datasource-rest')
 
 class CategoryAPI extends RESTDataSource {
 
   async getChannels() {
     let rawChannels = await this.get(`${Category.categoryWithBookCount}`)
-    let channelMale = {gender: 'male', categories: rawChannels['male']}
-    let channelFemale = {gender: 'female', categories: rawChannels['female']}
-    return [channelMale, channelFemale]
+    let channels = {
+      male: rawChannels.male.map(category => (category.type = "male", category)),
+      female: rawChannels.female.map(category => (category.type = "female", category)),
+      picture: rawChannels.picture.map(category => (category.type = "picture", category)),
+      press: rawChannels.press.map(category => (category.type = "press", category)),
+    }
+    return channels
   }
 
   async getSubCategories() {
-    let rawSubs = await this.get(Category.categoryWithSubCategories)
-    return [...rawSubs['male'], ...rawSubs['female']]
+    return await this.get(Category.categoryWithSubCategories)
   }
 }
 
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {ApolloServer, gql} = require('apollo-server')
+const { ApolloServer, gql } = require('apollo-server')
 const CategoryAPI = require('./api/CategoryAPI')
 const BookAPI = require('./api/BookAPI')
 const RankAPI = require('./api/RankAPI')
@@ -6,7 +6,7 @@ const RankAPI = require('./api/RankAPI')
 const typeDefs = gql`
 
   type Query {
-    channels: [Channel]
+    channel: Channel!
     book(id: String!): Book
     chapter(id: String!): Chapter
     search(keyword: String!): [Book]!
@@ -18,11 +18,14 @@ const typeDefs = gql`
   }
 
   type Channel {
-    gender: String!
-    categories: [Category]!
+    male: [Category]!
+    female: [Category]!
+    picture: [Category]!
+    press: [Category]!
   }
 
   type Category {
+    type: String!
     name: String!
     bookCount: Int!
     bookCover: [String]!
@@ -119,62 +122,62 @@ const typeDefs = gql`
 
 const resolvers = {
   Mutation: {
-    addBook: (root, {title, author}) => {
-      let book = {title, author}
+    addBook: (root, { title, author }) => {
+      let book = { title, author }
       books.push(book)
       return book
     }
   },
   Query: {
-    channels: async (_parent, _args, {dataSources}) => {
+    channel: async (_parent, _args, { dataSources }) => {
       return await dataSources.categoryAPI.getChannels()
     },
-    book: async (_parent, {id}, {dataSources}) => {
+    book: async (_parent, { id }, { dataSources }) => {
       return await dataSources.bookAPI.getBookById(id)
     },
-    chapter: async (_parent, {id}, {dataSources}) => {
+    chapter: async (_parent, { id }, { dataSources }) => {
       let rawChapterContent = await dataSources.bookAPI.getChapterContent(id)
       return rawChapterContent.chapter
     },
-    search: async (_parent, {keyword}, {dataSources}) => {
+    search: async (_parent, { keyword }, { dataSources }) => {
       let rawSearch = await dataSources.bookAPI.fuzzySearch(keyword)
       return rawSearch.books
     },
-    ranks: async (_parent, _, {dataSources}) => {
+    ranks: async (_parent, _, { dataSources }) => {
       return await dataSources.rankAPI.getRanks()
     }
   },
   Category: {
-    mins: async (category, _args, {dataSources}) => {
+    mins: async (parent, _args, { dataSources }) => {
       let categories = await dataSources.categoryAPI.getSubCategories()
-      return categories.filter(cat => cat.major === category.name)[0].mins
+      return categories[parent.type].find(cat => cat.major === parent.name).mins || []
     }
   },
   Book: {
-    _sourceId: async (book, _args, {dataSources}) => {
+    _sourceId: async (book, _args, { dataSources }) => {
       let sources = await dataSources.bookAPI.getBookSource(book._id)
       return sources[0]._id
     },
-    relatedBooks: async (book, _args, {dataSources}) => {
+    relatedBooks: async (book, _args, { dataSources }) => {
       let rawRelated = await dataSources.bookAPI.getRelatedBooks(book._id)
       return rawRelated.books
     },
-    authorBooks: async (book, _args, {dataSources}) => {
+    authorBooks: async (book, _args, { dataSources }) => {
       let rawAuthor = await dataSources.bookAPI.getAuthorBooks(book._id)
       return rawAuthor.books
     },
-    chapters: async (book, _args, {dataSources}) => {
+    chapters: async (book, _args, { dataSources }) => {
       let sources = await dataSources.bookAPI.getBookSource(book._id)
       let rawChapters = await dataSources.bookAPI.getChapters(sources[0]._id)
       return rawChapters.chapters
     },
-    comments: async (book, _args, {dataSources}) => {
+    comments: async (book, _args, { dataSources }) => {
       let rawReviews = await dataSources.rankAPI.getShortReviews(book._id)
       return rawReviews.docs
     }
   },
   Rank: {
-    books: async (rank, _args, {dataSources}) => {
+    books: async (rank, _args, { dataSources }) => {
       let rawRank = await dataSources.rankAPI.getRankBooks(rank._id)
       return rawRank.ranking.books
     }
@@ -191,4 +194,4 @@ const server = new ApolloServer({
   })
 })
 
-server.listen().then(({url}) => console.log(`🚀  Server ready at ${url}`))
+server.listen().then(({ url }) => console.log(`🚀  Server ready at ${url}`))
